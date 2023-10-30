42d129a2a1b27ec173bca4ce25d30a4c983bb5f3
arunraj3 <arunrajgovindan2002@gmail.com>
2023-10-31 00:35:17 +0530

    Spring project initialized

---
+++ chat/src/main/java/com/real_time_chat_application/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package com.real_time_chat_application.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
